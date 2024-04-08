ecc414027903c0156af310340f394c2053788160
foreach-school <162062403+foreach-school@users.noreply.github.com>
2024-04-08 02:39:44 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,6 +44,21 @@ resource "aws_db_subnet_group" "subnet_group" {
   subnet_ids  = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 }
 
+resource "aws_db_parameter_group" "postgres_parameters" {
+  name   = "postgres-parameters"
+  family = "postgres14"
+
+  parameter {
+    name  = "max_connections"
+    value = "100"
+  }
+
+  parameter {
+    name  = "shared_buffers"
+    value = "512MB"
+  }
+}
+
 variable "db_password" {
   description = "Password for the database"
 }
@@ -61,7 +76,7 @@ resource "aws_db_instance" "postgres_tst" {
   instance_class        = "db.t3.small"
   username              = var.db_username
   password              = var.db_password
-  parameter_group_name  = "default.postgres12"
+  parameter_group_name  = aws_db_parameter_group.postgres_parameters.name
 
   vpc_security_group_ids = [aws_security_group.tf_sg.id]
   db_subnet_group_name = aws_db_subnet_group.subnet_group.name
